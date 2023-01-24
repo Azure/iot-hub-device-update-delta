77495cd05ceefceadb276e881fc703f13be6c3d5
MarkZagorski-msft <107247338+MarkZagorski-msft@users.noreply.github.com>
2023-01-24 06:00:10 -0800

    Update README and fix build issues for arm in vcpkg packages (#9)
    * Fix build for vcpkg packages.
    
    * Update README

--- copy_all_repo_files.ps1
+++ copy_all_repo_files.ps1
@@ -1,15 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-param ([string] $Source, [string] $Destination)
+param ([Parameter(Mandatory=$true,ValueFromPipeline=$false)][string] $Source, [Parameter(Mandatory=$true,ValueFromPipeline=$false)][string] $Destination)
 
-copy "$Source/*" $Destination
-
-function MigrateFolder([string]$source, [string]$destination)
-{
-    $source = $source.replace("/", "\");
-    $destination = $destination.replace("/", "\");
-    echo "Calling robocopy.exe $source $destination /e /purge"
-    & robocopy.exe $source $destination /e /purge
-}
-
-ls . -ad | ForEach-Object -Process { $subfolder = $_.Name; MigrateFolder "$Source/$subfolder" "$Destination/$subfolder" }
+& robocopy.exe $Source $Destination /e /purge /xd .git
