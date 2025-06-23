30e0f68290e7d79465d763a7ab582896238a6002
Mark Zagorski (from Dev Box) <markzag@microsoft.com>
2025-06-23 15:56:29 -0700

    Don't copy over this file, the .gitattributes (for LFS) or the preview folder.

--- copy_all_repo_files.ps1
+++ copy_all_repo_files.ps1
@@ -2,4 +2,4 @@
 # Licensed under the MIT License.
 param ([Parameter(Mandatory=$true,ValueFromPipeline=$false)][string] $Source, [Parameter(Mandatory=$true,ValueFromPipeline=$false)][string] $Destination)
 
-& robocopy.exe $Source $Destination /e /purge /xd .git
+& robocopy.exe $Source $Destination /e /purge /xd .git /xd preview /xf copy_all_repo_files.ps1 /xf .gitattributes
